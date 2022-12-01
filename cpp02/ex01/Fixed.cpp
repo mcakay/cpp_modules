cc908a102509275995a819d68fc7a1d9634cf188
Melih Cakay <101070694+mcakay@users.noreply.github.com>
2022-12-02 02:59:04 +0300

    cpp02/ex01 added

---
+++ cpp02/ex01/Fixed.cpp
@@ -0,0 +1,53 @@
+#include "Fixed.hpp"
+
+Fixed::Fixed()
+{
+	std::cout << "Default constructor called" << std::endl;
+	value = 0;
+}
+
+Fixed::~Fixed()
+{
+	std::cout << "Destructor called" << std::endl;
+}
+
+Fixed::Fixed(const Fixed &copy)
+{
+	std::cout << "Copy constructor called" << std::endl;
+	*this = copy;
+}
+
+Fixed::Fixed(const int intVal)
+{
+	std::cout << "Int constructor called" << std::endl;
+	value = intVal * 256;
+}
+
+Fixed::Fixed(const float floatVal)
+{
+	std::cout << "Float constructor called" << std::endl;
+	value = roundf(floatVal * 256);
+}
+
+Fixed &Fixed::operator=(const Fixed &number)
+{
+	std::cout << "Copy assigment operator called" << std::endl;
+	this->value = number.value;
+	return (*this);
+}
+
+int Fixed::toInt() const
+{
+	return (value / 256);
+}
+
+float Fixed::toFloat() const
+{
+	return (float(value) / 256);
+}
+
+std::ostream &operator<<(std::ostream &output, const Fixed &fixed)
+{
+	output << fixed.toFloat();
+	return (output);
+}
