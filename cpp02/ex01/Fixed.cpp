221a47ee145977d9dc69720bd3c1addb13b4faf4
Melih Cakay <101070694+mcakay@users.noreply.github.com>
2022-12-02 11:08:35 +0300

    cpp02/ex01 getter setter

--- cpp02/ex01/Fixed.cpp
+++ cpp02/ex01/Fixed.cpp
@@ -51,3 +51,15 @@ std::ostream &operator<<(std::ostream &output, const Fixed &fixed)
 	output << fixed.toFloat();
 	return (output);
 }
+
+int Fixed::getRawBits(void) const
+{
+	std::cout << "getRawBits member function called" << std::endl;
+	return value;
+}
+
+void Fixed::setRawBits(int const raw)
+{
+	std::cout << "setRawBits member function called" << std::endl;
+	value = raw;
+}
