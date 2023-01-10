cd462a84cb93258955352a947216334ba00e684a
Melih Cakay <101070694+mcakay@users.noreply.github.com>
2023-01-10 17:04:30 +0300

    cpp04/ex01 update

--- cpp04/ex01/main.cpp
+++ cpp04/ex01/main.cpp
@@ -11,13 +11,15 @@ int	main(void)
 		else
 			animals[i] = new Cat();
 	}
+	Animal *copy = new Animal((Animal&)animals[0]);
 	for (int i = 0; i < 4; i++)
 	{
 		animals[i]->makeSound();
 	}
+	std::cout << "animals[0] address: " << animals[0] << std::endl;
+	std::cout << "copy address: " << copy << std::endl;
 	for (int i = 0; i < 4; i++)
 	{
-		
 		delete animals[i];
 	}
 	return (0);
