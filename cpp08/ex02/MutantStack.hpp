15b7a1afd6c40150ad0487dc8097054d22d862af
Melih Cakay <101070694+mcakay@users.noreply.github.com>
2023-01-15 00:59:13 +0300

    cpp08/ex02 added

---
+++ cpp08/ex02/MutantStack.hpp
@@ -0,0 +1,28 @@
+#ifndef MUTANTSTACK_HPP
+# define MUTANTSTACK_HPP
+
+# include <stack>
+# include <iostream>
+
+template <typename T>
+class MutantStack : public std::stack<T>
+{
+	public:
+		MutantStack() : std::stack<T>(){}
+		MutantStack &operator=(const MutantStack &other)
+		{
+			std::stack<T>::operator=(other);
+			return *this;
+		}
+		~MutantStack() {}
+		MutantStack(const MutantStack &other) : std::stack<T>(other) {}
+		typedef typename std::stack<T>::container_type::iterator iterator;
+		iterator begin() { 
+			return std::stack<T>::c.begin(); 
+		}
+		iterator end() { 
+			return std::stack<T>::c.end(); 
+		}
+};
+
+#endif
